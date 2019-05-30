2c202cbaff785fb07e0b116961bc97cefc5fbe2d
Lucas Ribino <lribino@outlook.com>
2019-05-30 20:32:05 -0300

    Delete lista.py

--- lista.py
+++
@@ -1,9 +0,0 @@
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-altura = float(input("Digite seu altura: "))
-peso = float(input("Digite seu peso: "))
-op = bool(input("Estado Civil\n1. Casado\n2. Solteiro\n"))
-
-eu = [nome,idade,altura,peso,op]
-
-print(eu[3:5])
